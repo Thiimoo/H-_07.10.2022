6484b4bae60b0442ace6165914adcc3fd8e3e23a
Thiimoo <timo.reitboeck@gmx.at>
2022-10-07 12:20:33 +0200

    project completed 100%

--- Hue.07.10.2022/src/JavaStreamsTester.java
+++ Hue.07.10.2022/src/JavaStreamsTester.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaStreamsTester {
 
     private static List<String> stringList = new ArrayList<>();
+    private static List<Integer> intsList = new ArrayList<>();
 
     public static void main(String[] args) {
         stringList.add("yes");
@@ -12,7 +18,7 @@ public class JavaStreamsTester {
         stringList.add("");
         stringList.add("help");
         stringList.add("");
-        System.out.println("List:");
+        System.out.println("stringsList:");
         stringList.forEach(System.out::println);
         System.out.println("\n");
 
@@ -24,6 +30,34 @@ public class JavaStreamsTester {
         System.out.println("\n");
         System.out.println("without empty Strings:");
         deleteEmptyStrings().forEach(System.out::println);
+        System.out.println("\n");
+        System.out.println("merged Strings:");
+        System.out.println(getMergedString(";"));
+        System.out.println("\n");
+
+        intsList.add(1);
+        intsList.add(2);
+        intsList.add(3);
+        intsList.add(4);
+        intsList.add(5);
+        System.out.println("intsList:");
+        intsList.forEach(System.out::println);
+        System.out.println("\n");
+
+        System.out.println("squared ints:");
+        getSquares().forEach(System.out::println);
+        System.out.println("\n");
+        System.out.println("maximum:");
+        System.out.println(getMax());
+        System.out.println("\n");
+        System.out.println("minimum:");
+        System.out.println(getMin());
+        System.out.println("\n");
+        System.out.println("sum:");
+        System.out.println(getSum());
+        System.out.println("\n");
+        System.out.println("average:");
+        System.out.println(getAverage());
         System.out.println("\n");
     }
 
@@ -41,6 +75,38 @@ public class JavaStreamsTester {
     {
         stringList.removeIf(String::isEmpty);
         return stringList;
+    }
+
+    private static String getMergedString(String separator)
+    {
+        String result = stringList.stream().collect(Collectors.joining(separator));
+        return result;
+    }
+
+    private static List<Integer> getSquares()
+    {
+        return intsList.stream().map(m->m*m).toList();
+    }
+
+    private static int getMax()
+    {
+        return intsList.stream().mapToInt(m -> m).max().orElseThrow(NoSuchElementException::new);
+    }
+
+    private static int getMin()
+    {
+        return intsList.stream().mapToInt(m -> m).min().orElseThrow(NoSuchElementException::new);
+    }
+    private static int getSum()
+    {
+        return intsList.stream().mapToInt(m -> m).sum();
+    }
+    private static int getAverage()
+    {
+        int[] array = new int[intsList.size()];
+        for(int i = 0; i < intsList.size(); i++) array[i] = intsList.get(i);
+        IntStream stream = Arrays.stream(array);
+        return (int) stream.average().getAsDouble();
     }
 
 }
