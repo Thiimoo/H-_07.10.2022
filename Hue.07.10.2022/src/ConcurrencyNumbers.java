e6c66909a4efd18fc17ec47a02ead83234fa4b0c
Thiimoo <timo.reitboeck@gmx.at>
2022-10-07 10:18:37 +0200

    half 1 bsp readFile

--- Hue.07.10.2022/src/ConcurrencyNumbers.java
+++ Hue.07.10.2022/src/ConcurrencyNumbers.java
@@ -1,2 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ConcurrencyNumbers {
+
+    public List<Integer> readFile()
+    {
+        String line;
+        String[] parts;
+        List<Integer> integerList = new ArrayList<>();
+        try{
+            Scanner s = new Scanner(new File("scr/numbers.csv"));
+            while (s.hasNextLine())
+            {
+                line = s.nextLine();
+                parts = line.split(":");
+                for (int i = 0; i < parts.length; i++) {
+                    if (parts[i] != null)
+                    {
+                        if (isNumeric(parts[i]))
+                        {
+                            integerList.add(Integer.parseInt(parts[i]));
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return integerList;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 }
---
+++ Hue.07.10.2022/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input chunks:");
+        int chunks = s.nextInt();
+        System.out.println("Input divider:");
+        int divider = s.nextInt();
+    }
+
+}
